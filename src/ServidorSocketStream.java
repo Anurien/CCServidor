2871b6e2850839d49b1c452e9f155e8fe678a3c9
Nuria <ngarciacobas@danielcastelao.org>
2023-01-27 10:50:16 +0100

    Añadido case de las opciones de los botones al servidor

--- src/ServidorSocketStream.java
+++ src/ServidorSocketStream.java
@@ -30,6 +30,31 @@ public class ServidorSocketStream {
             byte[] mensaje = new byte[25];
             is.read(mensaje);
 
+            /*InputStream entrada = nuevoSocket.getInputStream();
+            OutputStream salida = nuevoSocket.getOutputStream();*/
+
+            int resultado;
+            int operador = 6;
+            int[] operando = new int[10];
+            switch (operador) {
+                case 1:
+                    resultado = operando[0] / 120;
+                    System.out.println("El resultado es : " + resultado + "campos de fútbol");
+                case 2:
+                    resultado = 420 - operando[0];
+                    System.out.println("Te quedan " + resultado + "meses para llegar a la jubilación completa");
+                case 3:
+                    resultado = operando[0] / 34;
+                    System.out.println("El resultado es : " + resultado + "libros de Pérez-Reverte");
+                case 4:
+                    resultado = (int) (operando[0] - 1.397);
+                    System.out.println("La diferencia es de : " + Math.abs(resultado) + "€");
+                case 5:
+                    newSocket.close();
+                    serverSocket.close();
+
+            }
+
             System.out.println("Mensaje recibido: " + new String(mensaje));
 
             System.out.println("Cerrando el nuevo socket");
