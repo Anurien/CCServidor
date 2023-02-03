60eb385c2b5fff18b81a6718dc1aff1f7f9692cf
Nuria <ngarciacobas@danielcastelao.org>
2023-02-03 11:57:06 +0100

    no se que hice algo importante seguro

--- src/ServidorSocketStream.java
+++ src/ServidorSocketStream.java
@@ -29,49 +29,43 @@ public class ServidorSocketStream {
             DataOutputStream sos = new DataOutputStream(os);
 
             int mensaje = eis.readInt();
-            int operador = mensaje;
-
 
             int resultado;
             System.out.println("Esperando al 1er numero...");
             int operando = eis.readInt();
             System.out.println("El numero recibido es: " + operando);
-            switch (operador) {
+            switch (mensaje) {
                 case 1 -> {
                     resultado = operando / 34;
                     System.out.println("El resultado es : " + resultado + " libros de Pérez-Reverte");
-                    sos.writeInt(1);
                     sos.writeUTF("El resultado es : " + resultado + " libros de Pérez-Reverte");
                 }
                 case 2 -> {
                     resultado = operando / 120;
                     System.out.println("El resultado es : " + resultado + " campos de fútbol");
-                    sos.writeInt(2);
                     sos.writeUTF("El resultado es : " + resultado + " campos de fútbol");
                 }
                 case 3 -> {
-                    resultado = 420 - operando;
+                    resultado = 420 - operando * 12;
                     System.out.println("Te quedan " + resultado + " meses para llegar a la jubilación completa");
-                    sos.writeInt(3);
                     sos.writeUTF("Te quedan " + resultado + " meses para llegar a la jubilación completa");
                 }
                 case 4 -> {
                     resultado = (int) (operando - 1.397);
                     System.out.println("La diferencia es de : " + Math.abs(resultado) + " €");
-                    sos.writeInt(4);
                     sos.writeUTF("La diferencia es de : " + Math.abs(resultado) + " €");
                 }
                 case 5 -> {
                     System.out.println("Cerrando el nuevo socket");
-                    newSocket.close();
+                    //newSocket.close();
                     System.out.println("Cerrando el socket servidor");
-                    serverSocket.close();
+                    //serverSocket.close();
                     System.out.println("Terminado");
                 }
             }
 
 
-        } catch (IOException e) {
+        } catch (IOException ignored) {
         }
     }
 }
